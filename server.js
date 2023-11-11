1590e294bfde3fc75ae0b06d0d4755cbc435dd9e
Minji Park <gbsmj0312@gmail.com>
2023-11-11 22:14:18 +0900

    Edit: comment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { Server } = require('socket.io');
 async function server(){
     const http = require('http').createServer(app);
     const io = new Server(http, {transports: ['websocket']});
-    const roomName = 'dogfoot';
+    const roomName = 'p2p';
     io.on('connection', (socket) => {
         console.log('User connected');
         socket.on('disconnect', () => {
@@ -16,20 +16,20 @@ async function server(){
             socket.to(roomName).emit('joined');
         });
         socket.on('offer', (offer) => {
-            console.log('Offer received:', offer);
+            console.log('Offer received:', socket.id);
             socket.to(roomName).emit('offer', offer);
         });
         socket.on('answer', (answer) => {
-            console.log('Answer received:', answer);
+            console.log('Answer received:', socket.id);
             socket.to(roomName).emit('answer', answer);
         });
         socket.on('ice', (ice) => {
-            console.log('ICE candidate received:', ice);
+            console.log('ICE candidate received:', socket.id);
             socket.to(roomName).emit('ice', ice);
         });
 
     });
-    http.listen(3000, () => console.log('server open !!'));
+    http.listen(3000, () => console.log('server open!'));
 }
 
 server();
